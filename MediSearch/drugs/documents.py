356c4a5d1eb44cf9feb19d11bb4c556df3f9a814
shinwoong <devwoong@shinwoongs-MacBook-Pro.local>
2025-03-04 23:22:17 +0900

    create elasticsearch's index && Dockerfile for korean_analyzer && update index.html

--- MediSearch/drugs/documents.py
+++ MediSearch/drugs/documents.py
@@ -1,13 +1,30 @@
-from django_elasticsearch_dsl import Document
+from django_elasticsearch_dsl import Document, fields
 from django_elasticsearch_dsl.registries import registry
 from .models import Drug
 
 @registry.register_document
 class DrugDocument(Document):
+    prdlst_nm = fields.TextField(analyzer="korean_analyzer")
+    bssh_nm = fields.TextField(analyzer="korean_analyzer")
+    drug_cpnt_kor_nm = fields.TextField(analyzer="korean_analyzer")
+    pthd_nm = fields.TextField(analyzer="korean_analyzer")
+
     class Index:
         name = "drugs"
-        settings = {"number_of_shards": 1, "number_of_replicas": 0}
+        settings = {
+            "number_of_shards": 1,
+            "number_of_replicas": 0,
+            "analysis": {
+                "analyzer": {
+                    "korean_analyzer": {
+                        "type": "custom",
+                        "tokenizer": "nori_tokenizer",
+                        "filter": ["lowercase"]
+                    }
+                }
+            }
+        }
 
     class Django:
         model = Drug
-        fields = ["prdlst_nm", "drug_cpnt_kor_nm", "drug_cpnt_eng_nm", "bssh_nm", "pthd_nm", "ptnt_no", "ptnt_reg_dt"]
+        fields = ["drug_cpnt_eng_nm", "ptnt_no", "ptnt_reg_dt"]
--- MediSearch/drugs/views.py
+++ MediSearch/drugs/views.py
@@ -1,12 +1,40 @@
 from django.shortcuts import render
 from .documents import DrugDocument
-from elasticsearch_dsl.query import MultiMatch
+from elasticsearch_dsl import Q
+from .models import Drug
+
 
 def index(request):
-    q = request.GET.get("q")
+    q = request.GET.get("q") #질의어  
+    bssh_nm_filter = request.GET.get("bssh_nm") #특허권 등재자 필터 값
     context = {}
+    query = None
+    result = None
+    
+    # 키워드 검색 (q가 있을 경우)
     if q:
-        query = MultiMatch(query=q, fields=["prdlst_nm", "drug_cpnt_kor_nm", "drug_cpnt_eng_nm", "bssh_nm", "pthd_nm"])
-        s = DrugDocument.search().query(query)
-        context["drugs"] = s
+        query = Q("multi_match", query=q, fields=["prdlst_nm", "drug_cpnt_kor_nm", "drug_cpnt_eng_nm", "bssh_nm", "pthd_nm"]) | \
+                Q("match_phrase", prdlst_nm=q) | \
+                Q("term", drug_cpnt_eng_nm=q)
+        result = DrugDocument.search().query(query)
+    
+    # bssh_nm 필터가 있을 경우 (특허권 등재자 기준으로 필터링)
+    if bssh_nm_filter:
+        filter_query = Q("term", bssh_nm=bssh_nm_filter)
+        if result:  # 기존에 검색된 결과가 있으면 필터 추가
+            result = result.filter(filter_query)
+        else:  # 결과가 없다면 필터만 적용
+            result = DrugDocument.search().filter(filter_query)
+
+
+    context["drugs"] = result
+    if result:
+        for r in result:
+            print(r)
+    
+
+    # 특허권 등재자 목록을 가져와서 드롭다운 메뉴에 사용
+    unique_bssh_nm = Drug.objects.values('bssh_nm').distinct()
+    context["bssh_nm_list"] = [entry['bssh_nm'] for entry in unique_bssh_nm]
+
     return render(request, 'drugs/index.html', context)
